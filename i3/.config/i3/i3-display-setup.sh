71e116c10a920b454aecaf90fe2c63fafeb5223c
Rory Houlihan <rhoulihan@protonmail.com>
2021-10-08 16:27:13 +0200

    add i3 folder

---
+++ i3/.config/i3/i3-display-setup.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+if [ $(xrandr | grep HDMI-1 | awk '{ print $2 }') = "disconnected" ]; then
+   xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
+else
+   xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1 --off --output HDMI-1 --primary --mode 3840x2160 --pos 1920x0 --rotate normal --output DP-2 --off
+fi
+
+feh --bg-scale /home/rhoulihan/Pictures/wallpapers/aenami-1633200656101-9566.jpg
+picom --config /home/rhoulihan/.config/picom/picom.conf
---
+++ i3/.config/i3/lock.sh
@@ -0,0 +1,6 @@
+TMPBG=/tmp/screen.png
+scrot $TMPBG
+convert $TMPBG -scale 10% -scale 1000% $TMPBG
+convert $TMPBG -composite -matte $TMPBG
+i3lock -i $TMPBG
+rm $TMPBG
