2687e708566ce56390b1c6adedde697dccf0ac32
Rory Houlihan <rhoulihan@protonmail.com>
2021-10-08 16:33:27 +0200

    add polybar

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+if [ $(xrandr | grep HDMI-1 | awk '{ print $2 }') = "disconnected" ]; then
+    export MONITOR=eDP-1
+else
+    export MONITOR=HDMI-1
+fi
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar top-main &
+polybar bottom &
