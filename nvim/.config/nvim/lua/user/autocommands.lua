f401d70b8b88e69a71ab94ef4d228b0452de7ebb
Rory Houlihan <rhoulihan@protonmail.com>
2022-07-27 11:58:54 +0200

    add autocommand for removing trailing whitespace on save.

--- nvim/.config/nvim/lua/user/autocommands.lua
+++ nvim/.config/nvim/lua/user/autocommands.lua
@@ -5,21 +5,9 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function() vim.highlight.on_yank() end,
 })
 
--- vim.api.nvim_create_augroup("vdiff_colors", { clear = true })
--- vim.api.nvim_create_autocmd("BufNewFile", {
---     group = "vdiff_colors",
---     callback = function()
---         if vim.api.nvim_win_get_option(0, "diff")
---         then
---             vim.cmd "colorscheme default"
---         else
---             vim.cmd "colorscheme gruvbox"
---             vim.cmd "set background=dark"
---             vim.cmd "set termguicolors"
---             vim.cmd "let g:limelight_conceal_ctermfg = 240"
---             vim.cmd "let g:limelight_conceal_guifg = '#777777'"
---             vim.cmd "hi! Normal ctermbg=NONE guibg=NONE"
---             vim.cmd "hi! NonText ctermbg=NONE guibg=NONE guifg=NONE ctermfg=NONE"
---         end
---     end
--- })
+vim.api.nvim_create_augroup("remove_whitespace", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+    group = "remove_whitespace",
+    pattern = "*",
+    command = [[:keepjumps keeppatterns %s/\s\+$//e]]
+})
