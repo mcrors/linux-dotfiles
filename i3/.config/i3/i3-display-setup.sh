0625e8881bb4ba795e6c4e8865ee816e78d49fc6
Rory Houlihan <rhoulihan@purestorage.com>
2022-10-04 16:16:29 +0200

    some i3 config changes. Add tsserver to nvim. oh my zsh update

--- i3/.config/i3/i3-display-setup.sh
+++ i3/.config/i3/i3-display-setup.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
-if [ $(xrandr | grep HDMI-1 | awk '{ print $2 }') = "disconnected" ]; then
-   xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
+if [ $(xrandr | grep DP-2 | awk '{ print $2 }') = "connected" ]; then
+    xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1 --off --output HDMI-1 --mode 3840x2160 --pos 5760x0 --rotate normal --output DP-2 --primary --mode 3840x2160 --pos 1920x0 --rotate normal
 else
-   MODE=$(xrandr | grep HDMI-1 | awk '{ print $3 }')
-   MODE=${MODE:0:9}
-   xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1 --off --output HDMI-1 --primary --mode $MODE --pos 1920x0 --rotate normal --output DP-2 --off
-   xrandr --output HDMI-1 --right-of eDP-1
+    if [ $(xrandr | grep HDMI-1 | awk '{ print $2 }') = "connected" ]; then
+        xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1 --off --output HDMI-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --off
+    else
+        xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off
+    fi
 fi
 
 WALLPAPERS_DIR=/home/rhoulihan/Pictures/wallpapers
