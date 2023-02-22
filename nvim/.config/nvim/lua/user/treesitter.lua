f55e81800c7e7575ba23bdf76062269b8943158c
Rory Houlihan <rhoulihan@purestorage.com>
2023-02-22 08:59:52 +0100

    turn on treesitter indent for python

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -31,5 +31,5 @@ configs.setup({
     autopairs = {
         enable = true,
     },
-    indent = { enable = true, disable = { "python", "css" } },
+    indent = { enable = true, disable = { "css" } },
 })
