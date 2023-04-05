1757fa8dd24b599d1a0b71bb69c335ed731f0012
Rory Houlihan <rhoulihan@purestorage.com>
2023-04-05 13:50:00 +0200

    remove screen bluring for lock screen

--- i3/.config/i3/lock.sh
+++ i3/.config/i3/lock.sh
@@ -1,6 +1,7 @@
-TMPBG=/tmp/screen.png
-scrot $TMPBG
-convert $TMPBG -scale 10% -scale 1000% $TMPBG
-convert $TMPBG -composite -matte $TMPBG
-i3lock -i $TMPBG
-rm $TMPBG
+#TMPBG=/tmp/screen.png
+#scrot $TMPBG
+#convert $TMPBG -scale 10% -scale 1000% $TMPBG
+#convert $TMPBG -composite -matte $TMPBG
+LOCKBG=/home/rhoulihan/Pictures/Pure-Storage-FlashArrayXL.png
+i3lock -i $LOCKBG
+#rm $TMPBG
