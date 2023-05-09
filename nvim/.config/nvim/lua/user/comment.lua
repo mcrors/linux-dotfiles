378a03a894244eb32ed8241b430b8a8b21227bb1
Rory Houlihan <rhoulihan@purestorage.com>
2023-05-09 09:19:08 +0200

    change nvim comment keymappings

--- nvim/.config/nvim/lua/user/comment.lua
+++ nvim/.config/nvim/lua/user/comment.lua
@@ -3,4 +3,9 @@ if not nvim_comment_status_ok then
   return
 end
 
-nvim_comment.setup()
+nvim_comment.setup(
+    {
+        line_mapping = "<leader>cl",
+        operator_mapping = "<leader>cm"
+    }
+)
