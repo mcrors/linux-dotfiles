fdb0c728f3806d1ad3b1c27e26641a9885ef39fc
Rory Houlihan <rhoulihan@purestorage.com>
2023-05-22 09:19:55 +0200

    git config and nvim stuff

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -72,6 +72,8 @@ keymap("n", "<leader>gf", "<cmd>lua require('telescope.builtin').git_files()<CR>
 keymap("n", "<C-f>", "<cmd>lua require('telescope.builtin').live_grep()<CR>", opts)
 keymap("n", "<leader>to", "<cmd>lua require('telescope.builtin').treesitter(require('telescope.themes').get_dropdown())<CR>", opts)
 keymap("n", "gr", "<cmd>lua require('telescope.builtin').lsp_references(require('telescope.themes').get_ivy())<CR>", opts)
+keymap("n", "<leader>gk", "<cmd> lua require('telescope.builtin').keymaps()<CR>", opts)
+keymap("n", "<leader>gw", "<cmd> lua require('telescope.builtin').git_branches()<CR>", opts)
 
 -- Debugger mappings
 keymap("n", "<F5>", "<cmd>lua require('dap').continue()<CR>", opts)
