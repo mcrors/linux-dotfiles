502548d7c56020ccb9cb3a44357554f5da908773
Rory Houlihan <rhoulihan@purestorage.com>
2023-06-26 13:58:27 +0200

    add some terraformls config

--- nvim/.config/nvim/lua/user/lsp/settings/terraformls.lua
+++ nvim/.config/nvim/lua/user/lsp/settings/terraformls.lua
@@ -1,1 +1,8 @@
-return { }
+local util = require "lspconfig/util"
+return {
+  default_config = {
+    cmd = { 'terraform-ls', 'serve' },
+    filetypes = { 'terraform', 'terraform-vars' },
+    root_dir = util.root_pattern('.terraform', '.git'),
+  },
+}
