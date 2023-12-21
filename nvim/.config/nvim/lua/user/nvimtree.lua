96d6af3c85b59e369f94097e55b8ada468fdf319
Rory Houlihan <rhoulihan@purestorage.com>
2023-12-21 09:16:10 +0100

    remove deprecated api's

--- nvim/.config/nvim/lua/user/nvimtree.lua
+++ nvim/.config/nvim/lua/user/nvimtree.lua
@@ -53,12 +53,12 @@ nvim_tree.setup {
     },
     disable_netrw = true,
     hijack_netrw = true,
-    open_on_setup = false,
-    ignore_ft_on_setup = {
-        "startify",
-        "dashboard",
-        "alpha",
-    },
+    -- open_on_setup = false,
+    -- ignore_ft_on_setup = {
+        -- "startify",
+        -- "dashboard",
+        -- "alpha",
+    -- },
     open_on_tab = false,
     hijack_cursor = false,
     update_cwd = true,
@@ -83,7 +83,6 @@ nvim_tree.setup {
     },
     view = {
         width = 50,
-        height = 30,
         hide_root_folder = true,
         side = "left",
         mappings = {
