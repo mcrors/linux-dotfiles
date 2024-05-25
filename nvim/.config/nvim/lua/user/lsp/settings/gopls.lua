e7062e70bdd0f3697b350cc7a7be694307ab01b6
Rory Houlihan <rhoulihan@protonmail.com>
2024-05-25 18:01:32 +0200

    add some shit to do with codelens that I don't understand

--- nvim/.config/nvim/lua/user/lsp/settings/gopls.lua
+++ nvim/.config/nvim/lua/user/lsp/settings/gopls.lua
@@ -10,6 +10,13 @@ return {
           unusedparams = true,
         },
         staticcheck = true,
+        -- https://github.com/ray-x/go.nvim/issues/113
+        codelenses = {
+            generate = true, -- show the `go generate` lens.
+            gc_details = true, --  // Show a code lens toggling the display of gc's choices.
+            test = true,
+            tidy = true,
+        },
       },
     },
 }
