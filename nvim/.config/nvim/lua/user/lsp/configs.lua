d699b732dd3eb2e8d6f51e92e3769412cd30d8e1
Rory Houlihan <rhoulihan@protonmail.com>
2024-09-10 15:05:08 +0200

    update ts ls, update some vscode stuff

--- nvim/.config/nvim/lua/user/lsp/configs.lua
+++ nvim/.config/nvim/lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "pyright", "tsserver", "gopls", "lua_ls" }
+local servers = { "jsonls", "pyright", "ts_ls", "gopls", "lua_ls" }
 
 lsp_installer.setup({
 	ensure_installed = servers,
