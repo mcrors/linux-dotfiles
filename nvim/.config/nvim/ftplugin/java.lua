302659f06d93f17d79afe28c4b52404394706f5e
Rory Houlihan <rhoulihan@protonmail.com>
2024-11-14 17:14:39 +0100

    add some gradle trust stuff

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -11,4 +11,24 @@ local config = {
 }
 config.on_attach = require("user.lsp.handlers").on_attach
 config.capabilities = require("user.lsp.handlers").capabilities
+config.settings = {
+    java = {
+        format = {
+            settings = {
+                url = 'https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml',
+                profile = 'GoogleStyle'
+            }
+        },
+        imports = {
+            gradle = {
+                wrapper = {
+                    checksums = {
+                         { sha256 = '9a9afff59390e0cab2134d6113690dc6e25eb6b3b16b1288b441c4cbdfd97a09', allowed = true }
+                    }
+                }
+            }
+        }
+    }
+}
+
 require('jdtls').start_or_attach(config)
