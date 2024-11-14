ddd0ceef15159f4df458ad2071c99cbe32ce42b1
Rory Houlihan <rhoulihan@protonmail.com>
2024-11-14 17:10:54 +0100

    expand the homedir

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -1,9 +1,10 @@
+local home_dir = vim.fn.expand("~")
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-local workspace_dir = '/home/rhoulihan/.cache/jdtls/workspace/' .. project_name
+local workspace_dir = home_dir .. '/.cache/jdtls/workspace/' .. project_name
 
 local config = {
     cmd = {
-        '/home/rhoulihan/.local/bin/jdtls',
+        home_dir .. '/.local/bin/jdtls',
         '-data', workspace_dir,
     },
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
